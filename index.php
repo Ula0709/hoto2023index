a1f75edd60be81a99c35256a86e65cc2cfd89f87
Ula0709 <159502337+Ula0709@users.noreply.github.com>
2024-02-15 23:26:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-// Generate two random hexadecimal strings of length 20
-$rand1 = bin2hex(random_bytes(10));
-$rand2 = bin2hex(random_bytes(10));
+$bytes = openssl_random_pseudo_bytes(10);
+$rand = bin2hex($bytes);
+$bytes2 = openssl_random_pseudo_bytes(10);
+$rand2 = bin2hex($bytes2);
 
-// Construct the redirect URL with random parameters
-$redirectUrl = "https://dorsite-88mys2qnw-ula0709s-projects.vercel.app/indexpaginaprincipal.html/?param1=$rand1&param2=$rand2";
-
-// Perform the redirect
-exit()
+echo '<meta http-equiv="Refresh" content="0;URL=https://dorsite-88mys2qnw-ula0709s-projects.vercel.app/indexpaginaprincipal.html/ads/KyV1SHTKARjFSEwuEpEM?='.$rand.'=?&'.$rand2.'?=">';
 ?>
